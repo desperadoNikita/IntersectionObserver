a23312881ad16822e85a57e7265cd858adda806d
Desperado <113103241+desperadoNikita@users.noreply.github.com>
2023-06-12 15:03:51 +0300

    Add files via upload

---
+++ observer.js
@@ -0,0 +1,43 @@
+const cards = document.querySelectorAll('.card');
+const cardContainer = document.querySelector('.card-container');
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      entry.target.classList.toggle('show', entry.isIntersecting);
+      if (entry.isIntersecting) observer.unobserve(entry.target);
+    });
+  },
+  {
+    threshold: 1,
+  }
+);
+
+const lastElementObserver = new IntersectionObserver(entries => {
+  const lastCard = entries[0];
+  if (!lastCard.isIntersecting) return;
+  createNewCard();
+  lastElementObserver.unobserve(lastCard.target);
+  lastElementObserver.observe(document.querySelector('.card:last-child'));
+});
+
+lastElementObserver.observe(document.querySelector('.card:last-child'));
+
+cards.forEach(card => {
+  observer.observe(card);
+});
+
+function createNewCard() {
+  for (let index = 0; index <= 10; index++) {
+    const card = document.createElement('div');
+    card.textContent = 1993;
+    card.classList.add('card');
+    card.style.opacity = '0';
+    observer.observe(card);
+    cardContainer.append(card);
+
+    setTimeout(() => {
+      card.style.opacity = '1';
+    }, index * 100); 
+  }
+}
